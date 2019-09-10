26177ad4849cdd19ccf655d4b43983eb1ed18cd5
shun <prog.shun0830@gmail.com>
2019-09-10 18:45:33 +0900

    commitr

--- server.js
+++ server.js
@@ -11,16 +11,12 @@ app.get("/", function(req, res, next) {
     return res.render("top");
 });
 
-app.get("/nagata", function(req, res, next) {
+app.get("/eto", function(req, res, next) {
     return res.render("top");
 });
 
-app.get("/eto", function(req, res, next) {
-  return res.render("top");
-});
-
 app.get("/ikunobu", function(req, res, next) {
-  return res.render("top");
+    return res.render("top");
 });
 
 app.get("/shun", function(req, res, next) {
