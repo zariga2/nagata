ce67a3221be4852627c86891d38f2c743c97c767
zariga2 <nobusup123@yahoo.co.jp>
2019-09-10 17:41:39 +0900

    commit

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-let http = require('http');
-let express = require('express');
+let http = require("http");
+let path = require("path");
+let express = require("express");
 
-let app = express()
+let app = express();
 
-app.use(function (req, res, next) {
-    return res.send('Hello World');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res, next) {
+  return res.render("top");
+});
+
+app.get("/test", function(req, res, next) {
+  return res.render("test");
 });
 
 let server = http.createServer(app);
-server.listen('3000');
+server.listen("3000");
+console.log("サーバーを起動しました");
