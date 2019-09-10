ee09a31953c3678ac88f0c96c4f329cd7eaae83b
shun <prog.shun0830@gmail.com>
2019-09-10 17:44:14 +0900

    commit

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-let http = require('http');
-let express = require('express');
+let http = require("http");
+let express = require("express");
 
-let app = express()
+let app = express();
 
-app.use(function (req, res, next) {
-    return res.send('Hello World');
+app.use(function(req, res, next) {
+    return res.send("Hello World");
 });
 
 let server = http.createServer(app);
-server.listen('3000');
+server.listen("3000");
