2e95fa4fcb7fc52aa2c1ff91cbf0dc26425be020
zariga2 <nobusup123@yahoo.co.jp>
2019-09-11 12:24:51 +0900

    commit

--- server.js
+++ server.js
@@ -9,19 +9,10 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public/"));
 
 app.get("/", function(req, res, next) {
-    return res.render("top");
+  return res.render("top");
 });
-
-app.get("/eto", function(req, res, next) {
-    return res.render("top");
-});
-
-app.get("/ikunobu", function(req, res, next) {
-    return res.render("top");
-});
-
-app.get("/shun", function(req, res, next) {
-    return res.render("top");
+app.get("/header", function(req, res, next) {
+  return res.render("header");
 });
 
 let server = http.createServer(app);
