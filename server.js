d753248af3a5dc80fe8b3a115458edc8271309ec
shun <prog.shun0830@gmail.com>
2019-09-11 12:21:31 +0900

    ディレクトリ構成の雛形

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public/"));
 
+let top = require("./routes/top");
+app.use("/top", top);
+
 app.get("/", function(req, res, next) {
-    return res.render("top");
+    res.render("top");
 });
 
 app.get("/eto", function(req, res, next) {
