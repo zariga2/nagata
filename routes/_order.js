ab134b860f4ea64cd81d0aad3ebe057f25dcb595
shun <prog.shun0830@gmail.com>
2019-09-15 12:14:32 +0900

    bihin ページの作成

--- routes/_order.js
+++ routes/_order.js
@@ -6,11 +6,13 @@ const step1Siyou = require("./order/step1Siyou");
 const step2Haisousaki = require("./order/step2Haisousaki");
 const step3Hiyou = require("./order/step3Hiyou");
 const step4Kakunin = require("./order/step4Kakunin");
+const bihin = require("./order/bihin");
 
 // *  ⇒
 router.use("/step1Siyou", step1Siyou);
 router.use("/step2Haisousaki", step2Haisousaki);
 router.use("/step3Hiyou", step3Hiyou);
 router.use("/step4Kakunin", step4Kakunin);
+router.use("/bihin", bihin);
 
 module.exports = router;
