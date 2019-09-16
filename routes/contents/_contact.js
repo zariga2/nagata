836f505e552184448c0b3b3f4eb15823deeae0bd
shun <prog.shun0830@gmail.com>
2019-09-16 11:14:54 +0900

    /contact/toiawase のルーティング処理

--- routes/contents/_contact.js
+++ routes/contents/_contact.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 // *  ⇒
 const situmon = require("./contact/situmon");
+const toiawase = require("./contact/toiawase");
 
 // *  ⇒
 router.use("/situmon", situmon);
+router.use("/toiawase", toiawase);
 
 module.exports = router;
