db37698c5c6dc1fd5be41b610f6d87028b67d5fd
shun <prog.shun0830@gmail.com>
2019-09-17 15:53:08 +0900

    flow ページの作成

--- routes/contents/_flow.js
+++ routes/contents/_flow.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 // *  ⇒
 const nagare1 = require("./flow/nagare1");
+const nagare2 = require("./flow/nagare2");
 
 // *  ⇒
 router.use("/nagare1", nagare1);
+router.use("/nagare2", nagare2);
 
 module.exports = router;
